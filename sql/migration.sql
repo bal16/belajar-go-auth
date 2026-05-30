d0c2d882f744c2e410240b6273bdc01f2458d25e
bal16 <ahmadhamidbalya@gmail.com>
2026-05-30 21:14:24 +0700

    docs(sql): migration snapshot of RBAC

--- sql/migration.sql
+++ sql/migration.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS users (
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
--- Mengunci keunikan email di tingkat profil utama
+-- Locking email uniqueness at the main profile level
 CREATE UNIQUE INDEX IF NOT EXISTS idx_users_email_unique ON users (LOWER(email));
 
 
@@ -14,19 +14,19 @@ CREATE UNIQUE INDEX IF NOT EXISTS idx_users_email_unique ON users (LOWER(email))
 CREATE TABLE IF NOT EXISTS user_authentications (
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
-    provider VARCHAR(20) NOT NULL,      -- 'local' atau 'google'
-    provider_key VARCHAR(255) NOT NULL,  -- Jika 'local' diisi email, jika 'google' diisi Google UID
-    password_hash VARCHAR(255) NULL,    -- Hanya diisi jika provider = 'local'
+    provider VARCHAR(20) NOT NULL,      -- 'local' or 'google'
+    provider_key VARCHAR(255) NOT NULL,  -- If 'local' filled with email, if 'google' filled with Google UID
+    password_hash VARCHAR(255) NULL,    -- Only filled if provider = 'local'
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     
-    -- Relasi ke tabel users
+    -- relation to users
     CONSTRAINT fk_auth_user FOREIGN KEY (user_id) 
         REFERENCES users(id) ON DELETE CASCADE,
         
-    -- Aturan Bisnis 1: User tidak boleh punya lebih dari satu metode untuk provider yang sama
+    -- Bussiness Logic 1: User cannot have more than one method for the same provider
     CONSTRAINT uq_user_provider UNIQUE(user_id, provider),
     
-    -- Aturan Bisnis 2: Satu ID Google / satu email lokal tidak boleh dipakai oleh 2 akun yang berbeda
+    -- Business Logic 2: One Google ID TOKEN / one local email cannot be used by 2 different accounts
     CONSTRAINT uq_provider_key UNIQUE(provider, provider_key)
 );
 
@@ -36,28 +36,28 @@ CREATE TABLE IF NOT EXISTS user_refresh_tokens (
     id VARCHAR(255) PRIMARY KEY,
     user_id INT NOT NULL,
     token VARCHAR(255) NOT NULL,
-    device_info VARCHAR(255) NULL,       -- Menyimpan browser/nama device user agent
+    -- device_info VARCHAR(255) NULL,       -- Store the user agent browser/device name
     is_revoked BOOLEAN DEFAULT FALSE NOT NULL,
     expires_at TIMESTAMP WITH TIME ZONE NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     
-    -- Relasi ke tabel users
+    -- relation to users
     CONSTRAINT fk_refresh_user FOREIGN KEY (user_id) 
         REFERENCES users(id) ON DELETE CASCADE,
         
-    -- Token harus acak dan unik secara global
+    -- Token must be unique and globally random
     CONSTRAINT uq_refresh_token_string UNIQUE(token)
 );
 
 
--- Mempercepat query pencarian token saat frontend melakukan request POST /auth/refresh
+-- Speeding up token lookup queries when the frontend makes a POST /auth/refresh request
 CREATE INDEX IF NOT EXISTS idx_refresh_tokens_lookup ON user_refresh_tokens (id) WHERE is_revoked = FALSE;
 
--- Mempercepat proses pencarian relasi session per-user jika user ingin melihat daftar device aktif
+-- Speeding up the process of searching for session relationships per-user if the user wants to view a list of active devices
 CREATE INDEX IF NOT EXISTS idx_refresh_tokens_user_id ON user_refresh_tokens (user_id);
 
 
--- PostgreSQL membutuhkan fungsi khusus untuk memperbarui kolom updated_at secara otomatis
+-- PostgreSQL requires a special function to automatically update the updated_at column
 CREATE OR REPLACE FUNCTION update_modified_column()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -70,3 +70,83 @@ CREATE TRIGGER update_user_modtime
     BEFORE UPDATE ON users
     FOR EACH ROW
     EXECUTE FUNCTION update_modified_column();
+
+CREATE TABLE IF NOT EXISTS roles (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS user_roles (
+    user_id INT NOT NULL,
+    role_id INT NOT NULL,
+    PRIMARY KEY (user_id, role_id),
+    
+    CONSTRAINT fk_user_role_user FOREIGN KEY (user_id) 
+        REFERENCES users(id) ON DELETE CASCADE,
+        
+    CONSTRAINT fk_user_role_role FOREIGN KEY (role_id) 
+        REFERENCES roles(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS permissions (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS role_permissions (
+    role_id INT NOT NULL,
+    permission_id INT NOT NULL,
+    PRIMARY KEY (role_id, permission_id),
+    
+    CONSTRAINT fk_role_perm_role FOREIGN KEY (role_id) 
+        REFERENCES roles(id) ON DELETE CASCADE,
+        
+    CONSTRAINT fk_role_perm_permission FOREIGN KEY (permission_id) 
+        REFERENCES permissions(id) ON DELETE CASCADE
+);
+
+-- Seed initial roles and permissions
+INSERT INTO "roles" (name) VALUES ('super_admin'), ('admin'), ('user');
+
+INSERT INTO "permissions" (name) VALUES 
+    ('user.create'),
+    ('user.read'),
+    ('user.update'),
+    ('user.delete'),
+    ('role.create'),
+    ('role.read'),
+    ('role.update'),
+    ('role.delete'),
+    ('permission.create'),
+    ('permission.read'),
+    ('permission.update'),
+    ('permission.delete');
+
+
+
+
+INSERT INTO "role_permissions" (role_id, permission_id)
+-- 1. All permission for Super Admin
+SELECT 
+    (SELECT id FROM "roles" WHERE name = 'super_admin'), id 
+FROM "permissions"
+
+UNION ALL
+
+-- 2. Permission read & update for users & roles resource for Admin
+SELECT 
+    (SELECT id FROM "roles" WHERE name = 'admin'), id
+FROM "permissions"
+-- WHERE action IN ('read', 'update') AND resource IN ('users', 'roles')
+WHERE name IN ('user.read', 'user.update', 'role.read', 'role.update', 'permission.read', 'permission.update')
+
+UNION ALL
+
+-- 3. Permission read for users resource for regular User
+SELECT 
+    (SELECT id FROM "roles" WHERE name = 'user'), id
+FROM "permissions" 
+-- WHERE action = 'read' AND resource = 'users'
+WHERE name IN ('user.read', 'permission.read', 'role.read')
+
+ON CONFLICT DO NOTHING;
